3ea360f35e5ab5775ff11f166e744c9a7d14b53f
RajiaRani <Rajiarani.syal@gmail.com>
2025-06-10 12:28:00 -0500

    added

---
+++ frontend/src/landing_page/home/Hero.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Hero() {
+    return ( <>
+    Hero
+    </>);
+}
+
+export default Hero;
---
+++ frontend/src/landing_page/home/Pricing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Pricing() {
+    return ( <>
+    Price
+    </> );
+}
+
+export default Pricing;
