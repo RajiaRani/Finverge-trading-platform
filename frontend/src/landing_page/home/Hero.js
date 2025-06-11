0317dab83c9233f832d071cfd7f8bd89a903f1a9
RajiaRani <Rajiarani.syal@gmail.com>
2025-06-10 22:21:56 -0500

    added Home page

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,8 +1,16 @@
 import React from 'react';
+
 function Hero() {
     return (
         <>
-            <h1>Hero <i class="fa fa-address-book" aria-hidden="true"></i></h1>
+            <div className='container p-5'>
+                <div className='row text-center '>
+                    <img src="/media/images/homeHero.png" alt="HeroImage" className='mb-5' />
+                    <h1 className='mt-5'>Invest in everything</h1>
+                    <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                    <button className='btn btn-primary m-auto col-2'>Sign up for free</button>
+                </div>
+            </div>
         </>
     );
 }
--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -2,7 +2,32 @@ import React from 'react';
 function Stats() {
     return (
         <>
-            <h1>Stats</h1>
+         <div className='container'>
+          <div className='row'>
+           <div className='col-6'>
+             <h1>Trust with confidence</h1>
+
+             <h3>Customer-first always</h3>
+             <p>
+             That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.
+             </p>
+
+             <h3>No spam or gimmicks</h3>
+             <p>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. <a href="/">Our philosophies</a>.</p>
+
+             <h3>The Zerodha universe</h3>
+             <p>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+
+             <h3>Do better with money</h3>
+             <p>With initiatives like <a href="/">Nudge</a> and <a href="/">Kill Switch</a>, we don't just facilitate transactions, but actively help you do better with your money.</p>
+           </div>
+
+           <div className='col-6'>
+           <img src="/media/images/ecosystem.png" alt="Hero"  style={{ width: "100%", height:"100%"}}/>
+           </div>
+
+          </div>
+         </div>
         </>
     );
 }
