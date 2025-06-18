fbc2cf2c9510e536735def74e57d12947df63425
RajiaRani <Rajiarani.syal@gmail.com>
2025-06-18 13:54:29 -0500

    added

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -1,35 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Menu() {
-    const [isMenuOpen, setIsMenuOpen] = useState(0);
-    const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(0);
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
 
-    const hanldeMenuClick = (index) =>{
-     setIsMenuOpen(index);
-    }
+  const hanldeMenuClick = (index) => {
+    setIsMenuOpen(index);
+  };
 
-    const handleProfileClick = () => {
-        setIsProfileDropdownOpen(!isProfileDropdownOpen)
-    }
-    
-    return (
-        <>
-        <div className='menu-container d-flex'>
-            <img src='/logo.png' alt='logo' style={{width:"40px", height:"40px"}} />
-            <div className='menus'>
-                <ul className='d-flex' style={{listStyle:"none"}}>
-                    <li>Dashboard </li>
-                    <li> Orders</li>
-                    <li>Holdings </li>
-                    <li> Positions</li>
-                    <li>Funds </li>
-                    <li>Apps </li>
-                </ul>
+  const handleProfileClick = () => {
+    setIsProfileDropdownOpen(!isProfileDropdownOpen);
+  };
 
-            </div>
+  const menuClass = "menu";
+  const activemenuClass = "menu selected";
 
+  return (
+    <>
+      <div className="menu-container d-flex">
+        <img
+          src="/logo.png"
+          alt="logo"
+          style={{ width: "40px", height: "40px" }}
+        />
+        <div className="menus">
+          <ul className="d-flex" style={{ listStyle: "none" }}>
+            <li>
+              <Link to="/" onClick={() => hanldeMenuClick(0)}>
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to="/orders" onClick={() => hanldeMenuClick(1)}>
+                Orders
+              </Link>
+            </li>
+            <li>
+              <Link to="/holdings" onClick={() => hanldeMenuClick(2)}>
+                Holdings
+              </Link>{" "}
+            </li>
+            <li> 
+                <Link to="/positions" onClick={() => hanldeMenuClick(3)}>
+                Positions
+                </Link>
+            </li>
+            <li>
+                <Link to="/funds" onClick={() => hanldeMenuClick(4)}>
+                Funds
+                </Link> </li>
+            <li>
+                <Link to="/apps" onClick={() => hanldeMenuClick(5)}>
+                Apps
+                </Link> </li>
+          </ul>
         </div>
-        </>
-      );
+      </div>
+    </>
+  );
 }
 
 export default Menu;
