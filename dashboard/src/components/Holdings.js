247fd7c576a7584a88a7cb583841ebe464c18732
RajiaRani <Rajiarani.syal@gmail.com>
2025-06-20 14:53:20 -0500

    added

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
 function Holdings() {
-    return ( 
-        <>
-         <div className='container'>
-          Holiding
-          </div>
-        </>
-     );
+  return (
+    <>
+      <div>
+        <h3 className="title"> Holdings {allHoldings.length}</h3>
+        <div>
+          <table>
+            <tr>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Avg. cost</th>
+              <th>LTP</th>
+              <th>Cur. val</th>
+              <th>P&L</th>
+              <th>Net chg.</th>
+              <th>Day chg.</th>
+            </tr>
+
+            {allHoldings.map((stock,index) => {
+              const costPrice = stock.price * stock.qty;
+
+
+              })
+            }
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Holdings;
--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -97,10 +97,10 @@ function Menu() {
             </li>
           </ul>
           <hr />
-        <div className="profile" onClick={handleProfileClick}>
-          <div className="avatar">ZU</div>
-          <p className="username">USERID</p>
-        </div>
+          <div className="profile" onClick={handleProfileClick}>
+            <div className="avatar">ZU</div>
+            <p className="username">USERID</p>
+          </div>
         </div>
       </div>
     </>
