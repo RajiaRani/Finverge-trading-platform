4af60b5bf448ea8949117eaeee53c46372a82104
RajiaRani <Rajiarani.syal@gmail.com>
2025-08-30 12:24:24 -0500

    added

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -12,17 +12,18 @@ function Dashboard() {
   return (
     <>
       <div className="dashboard-container">
-        <WatchList />
-        
+  
+          <WatchList />
+ 
         <div className="content">
-            <Routes>
-                <Route exact path="/" element={<Summary/>} />
-                <Route  path="/orders" element={<Orders/>} />
-                <Route  path="/holdings" element={<Holdings/>} />
-                <Route  path="/positions" element={<Positions/>} />
-                <Route  path="/funds" element={<Funds/>} />
-                <Route  path="/apps" element={<App/>} />
-            </Routes>
+          <Routes>
+            <Route exact path="/" element={<Summary />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/holdings" element={<Holdings />} />
+            <Route path="/positions" element={<Positions />} />
+            <Route path="/funds" element={<Funds />} />
+            <Route path="/apps" element={<App />} />
+          </Routes>
         </div>
       </div>
     </>
